7ba09d13d630a6675c01c52b7e364069d1f7758a
shahidafridi2609 <74283932+shahidafridi2609@users.noreply.github.com>
2023-07-29 01:39:13 +0530

    removed unessassary

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Detect scroll event and change navbar background when scrolled
-window.addEventListener("scroll", () => {
-  const navbar = document.getElementById("navbar");
-  const scrollY = window.scrollY;
-
-  if (scrollY > 0) {
-    navbar.classList.add("navbar-scrolled");
-  } else {
-    navbar.classList.remove("navbar-scrolled");
-  }
-});
-
 function toggleMenu() {
   var navigation = document.querySelector(".navigation");
   navigation.classList.toggle("active");
